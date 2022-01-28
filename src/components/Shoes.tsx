d34c03c52c6a6237c506571fa512ee36b15785b5
Matheus Vieira <mvieira@venteclife.com>
2022-01-28 12:01:36 -0300

    Remove stage and add lights and loading

--- src/components/Shoes.tsx
+++ src/components/Shoes.tsx
@@ -1,27 +1,45 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { Canvas } from '@react-three/fiber';
-import { BakeShadows, OrbitControls, Stage } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
 import Shoe from './Shoe';
 import Text from './Text';
 import SkyBox from './SkyBox';
+
+function Loading() {
+  const ref = useRef<THREE.Mesh>();
+
+  useFrame(({ clock }) => {
+    ref.current!.rotation.y = clock.getElapsedTime();
+  });
+
+  return (
+    <mesh ref={ref} scale={0.5}>
+      <sphereBufferGeometry args={[1]} />
+      <meshStandardMaterial color="blue" roughness={0.75} wireframe />
+    </mesh>
+  );
+}
 
 function Shoes() {
   const { color } = useSelector((state: any) => state.shoe);
 
   return (
-    <Canvas shadows camera={{ fov: 35 }}>
+    <Canvas camera={{ fov: 40, position: [0, 3, 5] }}>
       <SkyBox />
-      <Suspense fallback={null}>
-        <Stage intensity={0.5} contactShadow={{ opacity: 0.7, blur: 2 }}>
-          <Shoe color={color} position={[0, 0, 0]} rotation={[0, -2, 0]} />
-          <Shoe color={color} position={[1, 0, 0]} rotation={[0, -2, Math.PI]} scale={-1} />
-          <Text text="Raptor" color="white" size={0.15} position={[0.1, -0.5, 1.1]} rotation={[-1.6, 0, -0.4]} />
-          <Text text="v1" color="#9d0e0e" size={0.05} position={[0.7, -0.5, 1.35]} rotation={[-1.6, 0, -0.4]} />
-        </Stage>
+      <ambientLight intensity={0.3} />
+      <directionalLight color="white" position={[0, 0, 5]} />
+      <Suspense fallback={<Loading />}>
+        <group position={[0, 0.5, 0]}>
+          <Shoe color={color} position={[-0.5, 0, 0]} rotation={[0, -2, 0]} />
+          <Shoe color={color} position={[0.5, 0, 0]} rotation={[0, -2, Math.PI]} scale={-1} />
+          <Text text="Space" color="white" size={0.15} position={[-0.5, -0.5, 1.1]} rotation={[-1.6, 0, -0.4]} />
+          <Text text="v1" color="#9d0e0e" size={0.05} position={[0.1, -0.5, 1.35]} rotation={[-1.6, 0, -0.4]} />
+        </group>
       </Suspense>
-      <BakeShadows />
       <OrbitControls autoRotate />
+      {/* <gridHelper /> */}
     </Canvas>
   )
 }
