1dba7f3238f54a16b3b45b426b0540d14c68b64c
Matheus Vieira <mvieira@venteclife.com>
2022-02-04 16:27:44 -0300

    Table texture uv mapping

--- src/components/UVMapping.tsx
+++ src/components/UVMapping.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useTexture, useProgress, Html } from '@react-three/drei';
+import { OrbitControls, useTexture, Html, useGLTF, Float, Backdrop } from '@react-three/drei';
 import * as THREE from 'three';
 
 function Cube() {
@@ -16,37 +16,52 @@ function Cube() {
   texture.center.set(0.5, 0.5);
   texture.rotation = THREE.MathUtils.degToRad(90);
 
-
   return (
-    <mesh>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial
-        side={THREE.DoubleSide}
-        map={texture}
-        alphaMap={texture}
-        transparent={true}
-        // displacementMap={texture}
-        // emissiveMap={texture}
-        // normalMap={texture}
-        // metalnessMap={texture2}
-        // roughnessMap={texture2}
-      />
-    </mesh>
+    <Float
+      speed={10}
+      rotationIntensity={1}
+      floatIntensity={2}
+    >
+      <mesh position={[0, 2.5, 0]}>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial
+          side={THREE.DoubleSide}
+          map={texture}
+          alphaMap={texture}
+          transparent={true}
+          // displacementMap={texture}
+          // emissiveMap={texture}
+          // normalMap={texture}
+          // metalnessMap={texture2}
+          // roughnessMap={texture2}
+        />
+      </mesh>
+    </Float>
   );
 }
 
-function Loader() {
-  const { progress } = useProgress();
-  return <Html center>{progress} % loaded</Html>;
+function Table() {
+  const gltf: any = useGLTF('/models/table.glb');
+
+  const texture = useTexture('/textures/table.png');
+  texture.flipY = false;
+
+  return (
+    <primitive object={gltf.nodes.Table} position={[-0.6, 0.35, 0]}>
+      <meshStandardMaterial map={texture} />
+    </primitive>
+  );
 }
 
 function UVMapping() {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [0, 4, 5] }}>
       <ambientLight intensity={1} />
       <OrbitControls autoRotate />
-      <Suspense fallback={<Loader />}>
+      <gridHelper />
+      <Suspense fallback={<Html center>loading...</Html>}>
         <Cube />
+        <Table />
       </Suspense>
     </Canvas>
   );
